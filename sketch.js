78f8a4dbbbeceb016b4dd8f74b883d59464b3653
rafael-mediodia <150557958+rafael-mediodia@users.noreply.github.com>
2024-01-28 20:11:46 -0500

    Fixed

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-let cellWidth = 40,
-  cellHeight = 40;
-//seperation of cell height and width to change the shape more
+let cellWidth = 100,
+  cellHeight = 100;
 let rows, cols;
 let shapes = [
   "town1",
@@ -17,21 +16,8 @@ let shapes = [
   "town12",
   "town13",
 ];
-let colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33F6", "#F6FF33"];
-let img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13;
-//set up global variables
+let img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13;
+
 function preload() {
   img1 = loadImage('town_house.JPG');
   img2 = loadImage('town_house2.JPG');
@@ -45,90 +31,75 @@ function preload() {
   img10 = loadImage('town_tree2.JPG');
   img11 = loadImage('town_tree.JPG');
   img12 = loadImage('town_bench.JPG');
-  img12 = loadImage('town_mailbox.JPG');
+  img13 = loadImage('town_mailbox.JPG');
 }
+
 function setup() {
-  //setting up operation to get an integer
   cols = Math.floor(windowWidth / cellWidth);
   rows = Math.floor(windowHeight / cellHeight);
   createCanvas(windowWidth, windowHeight, WEBGL);
-  //moves the rate of refresh
+  frameRate(3);
 }
 
 function draw() {
-  //continually executes code inside its block
-  //area where there is a big section of code is block
-  background(0,22);
-  // for meaning is a control flow statement that allows code to be executed routinely in a given condition
-  //3 components of for statement
-  //for(initilization statement, condition,)
-  //1 init is where you define and set the loop variable; in this case it would be i
-  //2 condition is a boolean expression which evaluates to true or false
-  for (let i = -9; i < cols; i = i + 1) {
-    for (let j = -9; j < rows; j = j + 1) {
+  background(0, 22);
+
+  for (let i = -9; i < cols; i++) {
+    for (let j = -9; j < rows; j++) {
       let x = i * cellWidth;
       let y = j * cellHeight;
       let shape = random(shapes);
-      let color = random(colors);
+      push();
+      translate(x, y);
+
       noFill();
-      //this is the conditional statement
+
       if (shape === "town1") {
-        translate(0, 0);
         texture(img1);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town2") {
-        translate(0, 0);
         texture(img2);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town3") {
-        translate(0, 0);
         texture(img3);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town4") {
-        translate(0, 0);
         texture(img4);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town5") {
-        translate(0, 0);
         texture(img5);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town6") {
-        translate(0, 0);
         texture(img6);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town7") {
-        translate(0, 0);
         texture(img7);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town8") {
-        translate(0, 0);
         texture(img8);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town9") {
-        translate(0, 0);
         texture(img9);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town10") {
-        translate(0, 0);
         texture(img10);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town11") {
-        translate(0, 0);
         texture(img11);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town12") {
-        translate(0, 0);
         texture(img12);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       } else if (shape === "town13") {
-        translate(0, 0);
         texture(img13);
-        rect(x, y, cellWidth, cellHeight);
+        rect(0, 0, cellWidth, cellHeight);
       }
+
+      pop();
     }
   }
 }
-//BOTH OF THESE FUNCTIONS MUST STAY!
+
 function mouseClicked() {
   setup();
 }
